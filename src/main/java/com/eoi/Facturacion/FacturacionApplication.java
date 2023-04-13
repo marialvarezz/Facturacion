4b16ea635df7143ffc201e552004a4730ae4f016
marialvarezz <123487313+marialvarezz@users.noreply.github.com>
2023-04-13 13:30:07 +0200

    FAC-4 API controller

--- src/main/java/com/eoi/Facturacion/FacturacionApplication.java
+++ src/main/java/com/eoi/Facturacion/FacturacionApplication.java
@@ -3,7 +3,7 @@ package com.eoi.Facturacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class FacturacionApplication {
 
 	public static void main(String[] args) {
